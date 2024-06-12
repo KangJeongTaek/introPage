0901458da24a5f63b10037596402cbb25dd8750e
KangJeongTaek <dfgddjd@naver.com>
2024-06-12 13:51:34 +0900

    initial commit

---
+++ src/main/java/com/example/kangjeongtaek/KangjeongtaekApplication.java
@@ -0,0 +1,13 @@
+package com.example.kangjeongtaek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KangjeongtaekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KangjeongtaekApplication.class, args);
+	}
+
+}
